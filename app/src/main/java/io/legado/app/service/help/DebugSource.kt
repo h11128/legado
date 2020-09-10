764b305648af4d06883440c2cce8473df40233e6
Jason Yao <h11129@gmail.com>
2020-09-09 23:08:49 -0400

    check source结合debug source的一些尝试
    check source结合debug source的一些尝试

--- app/src/main/java/io/legado/app/service/help/DebugSource.kt
+++ app/src/main/java/io/legado/app/service/help/DebugSource.kt
@@ -2,22 +2,69 @@ package io.legado.app.service.help
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import io.legado.app.App
 import io.legado.app.R
 import io.legado.app.constant.IntentAction
+import io.legado.app.data.entities.Book
 import io.legado.app.data.entities.BookSource
+import io.legado.app.data.entities.SearchBook
 import io.legado.app.help.coroutine.Coroutine
+import io.legado.app.model.Debug
 import io.legado.app.model.webBook.WebBook
 import io.legado.app.service.CheckSourceService
+import io.legado.app.utils.htmlFormat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.anko.toast
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.coroutines.CoroutineContext
 
-class DebugSource(val source: BookSource) {
+class DebugSource(val source: BookSource): Debug.Callback1 {
+
+    override fun printLog(state: Int, msg: String, source: BookSource) {
+        callback?.invoke(state, msg, source)
+    }
+
+    @Synchronized
+    fun log(
+        sourceUrl: String?,
+        msg: String? = "",
+        print: Boolean = true,
+        isHtml: Boolean = false,
+        showTime: Boolean = true,
+        state: Int = 1
+    ) {
+        if (callback == null || !print) return
+        var printMsg = msg ?: ""
+        if (isHtml) {
+            printMsg = printMsg.htmlFormat()
+        }
+        if (showTime) {
+            printMsg =
+                "${DEBUG_TIME_FORMAT.format(Date(System.currentTimeMillis() - startTime))} $printMsg"
+        }
+
+        if (sourceUrl != null && msg != null){
+            App.db.bookSourceDao().getBookSource(sourceUrl)?.let {
+                if (!it.searchUrl.isNullOrEmpty()) {
+                    printLog(state, msg, it)
+                    Log.d("Jason", "msg")
+                }
+            }
+        }
+    }
 
     companion object {
         var keyword = "都市"
+        fun observe(callback: (Int, String, BookSource)-> Unit){
+            this.callback = callback
+        }
+        private var startTime: Long = System.currentTimeMillis()
+        private val DEBUG_TIME_FORMAT = SimpleDateFormat("[mm:ss.SSS]", Locale.getDefault())
+        private var callback: ((Int, String, BookSource)-> Unit)? = null
 
         fun start(context: Context, sources: List<BookSource>) {
             if (sources.isEmpty()) {
@@ -49,14 +96,20 @@ class DebugSource(val source: BookSource) {
         onNext: (sourceUrl: String) -> Unit
     ): Coroutine<*> {
         val webBook = WebBook(source)
-        return webBook.searchBook(keyword, scope = scope, context = context).timeout(60000L)
+        val variableBook = SearchBook(origin = source.bookSourceUrl)
+        //Debug.simpleDebug(source, keyword)
+        log(source.bookSourceUrl, "校验搜索")
+        Log.d("Jason", source.bookSourceUrl.toString())
+        return webBook.searchBook(keyword, scope = scope, context = context, variableBook = variableBook).timeout(60000L)
             .timeout(60000L)
             .onError(Dispatchers.IO) {
                 source.addGroup("失效")
                 App.db.bookSourceDao().update(source)
+                log(source.bookSourceUrl, "校验失败")
             }.onSuccess(Dispatchers.IO) {
                 source.removeGroup("失效")
                 App.db.bookSourceDao().update(source)
+                log(source.bookSourceUrl, "校验成功")
             }.onFinally(Dispatchers.IO) {
                 onNext(source.bookSourceUrl)
             }
---
+++ app/src/main/java/io/legado/app/service/help/DebugSourceService.kt
@@ -0,0 +1,126 @@
+package io.legado.app.service.help
+
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import io.legado.app.App
+import io.legado.app.R
+import io.legado.app.base.BaseService
+import io.legado.app.constant.AppConst
+import io.legado.app.constant.IntentAction
+import io.legado.app.help.AppConfig
+import io.legado.app.help.IntentHelp
+import io.legado.app.help.coroutine.CompositeCoroutine
+import io.legado.app.ui.book.source.manage.BookSourceActivity
+import kotlinx.coroutines.asCoroutineDispatcher
+import org.jetbrains.anko.toast
+import java.util.concurrent.Executors
+import kotlin.math.min
+
+class DebugSourceService : BaseService() {
+    private var threadCount = AppConfig.threadCount
+    private var searchPool = Executors.newFixedThreadPool(threadCount).asCoroutineDispatcher()
+    private var tasks = CompositeCoroutine()
+    private val allIds = ArrayList<String>()
+    private val checkedIds = ArrayList<String>()
+    private var processIndex = 0
+
+    override fun onCreate() {
+        super.onCreate()
+        updateNotification(0, getString(R.string.start))
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            IntentAction.start -> intent.getStringArrayListExtra("selectIds")?.let {
+                check(it)
+            }
+            else -> stopSelf()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tasks.clear()
+        searchPool.close()
+    }
+
+    private fun check(ids: List<String>) {
+        if (allIds.isNotEmpty()) {
+            toast("已有书源在校验,等完成后再试")
+            return
+        }
+        tasks.clear()
+        allIds.clear()
+        checkedIds.clear()
+        allIds.addAll(ids)
+        processIndex = 0
+        threadCount = min(allIds.size, threadCount)
+        updateNotification(0, getString(R.string.progress_show, 0, allIds.size))
+        for (i in 0 until threadCount) {
+            check()
+        }
+    }
+
+    /**
+     * 检测
+     */
+    private fun check() {
+        val index = processIndex
+        synchronized(this) {
+            processIndex++
+        }
+        execute {
+            if (index < allIds.size) {
+                val sourceUrl = allIds[index]
+                App.db.bookSourceDao().getBookSource(sourceUrl)?.let { source ->
+                    if (source.searchUrl.isNullOrEmpty()) {
+                        onNext(sourceUrl)
+                    } else {
+                        DebugSource(source).check(this, searchPool) {
+                            onNext(it)
+                        }
+                    }
+                } ?: onNext(sourceUrl)
+            }
+        }
+    }
+
+    private fun onNext(sourceUrl: String) {
+        synchronized(this) {
+            check()
+            checkedIds.add(sourceUrl)
+            updateNotification(
+                checkedIds.size,
+                getString(R.string.progress_show, checkedIds.size, allIds.size)
+            )
+            if (processIndex >= allIds.size + threadCount - 1) {
+                stopSelf()
+            }
+        }
+    }
+
+    /**
+     * 更新通知
+     */
+    private fun updateNotification(state: Int, msg: String) {
+        val builder = NotificationCompat.Builder(this, AppConst.channelIdReadAloud)
+            .setSmallIcon(R.drawable.ic_network_check)
+            .setOngoing(true)
+            .setContentTitle(getString(R.string.check_book_source))
+            .setContentText(msg)
+            .setContentIntent(
+                IntentHelp.activityPendingIntent<BookSourceActivity>(this, "activity")
+            )
+            .addAction(
+                R.drawable.ic_stop_black_24dp,
+                getString(R.string.cancel),
+                IntentHelp.servicePendingIntent<DebugSourceService>(this, IntentAction.stop)
+            )
+        builder.setProgress(allIds.size, state, false)
+        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        val notification = builder.build()
+        startForeground(112202, notification)
+    }
+
+}
